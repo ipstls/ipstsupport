8ec24c35b76604af704469970094917f763b5eac
LearningSpace <learningspace.ipst@gmail.com>
2020-04-18 23:04:18 +0700

    fix

--- index.php
+++ index.php
@@ -10,16 +10,15 @@ include "linemessage.php";
 include "function.php";
 include "dialogflow.php"; 
 
-//Logs
-// $headers = getallheaders(); // ได้ค่า array ของส่วน Headers ทั้งหมดที่ Line ส่งมา
-// file_put_contents('headers.txt',json_encode($headers, JSON_PRETTY_PRINT)); 
-// file_put_contents('body.txt',file_get_contents('php://input')); // ส่วนของ body
-
 //กำหนด Path เก็บภาพที่ Server
 $datas = file_get_contents('php://input');
 /*Decode Json From LINE Data Body*/
 $deCode = json_decode($datas,true);
 file_put_contents('chat-log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+$token = "UjEiVgTUwEsD4D5FHSGLwY6+zVOR/bYumrBuvmFcgaPATMb/Lo4AmztbT95ygiPIt1/K9lHhm03EGlAnOR4HQ0yv0ltvcjP/c6HkejOwl+46KtzIfJI11BbpVoiWbjuVKqKTkdbomzF9SR2Zj7YM3gdB04t89/1O/w1cDnyilFU=";
+
+$LINEDatas['token'] = $token;
 
 if(count($deCode) == 2){
     //ดึงข้อมูลครั้งแรกเมื่อมีข้อความเข้ามาจาก Line
@@ -35,22 +34,16 @@ else if(count($deCode) == 4){
     $userId = $deCode['originalDetectIntentRequest']['payload']['data']['source']['userId'];
 }
 
-$token = "UjEiVgTUwEsD4D5FHSGLwY6+zVOR/bYumrBuvmFcgaPATMb/Lo4AmztbT95ygiPIt1/K9lHhm03EGlAnOR4HQ0yv0ltvcjP/c6HkejOwl+46KtzIfJI11BbpVoiWbjuVKqKTkdbomzF9SR2Zj7YM3gdB04t89/1O/w1cDnyilFU=";
-
-$LINEDatas['token'] = $token;
-
 //ตรวจสอบประเภทของข้อมูลที่ถูกส่งมาจาก Line
 if($messageType == 'text'){
     switch ($action) {
     case "OtherTypeConfirm":  
         $wait = "ระบบกำลังส่งอีเมลไปยังผู้ที่เกี่ยวข้องเรียบร้อยแล้ว เจ้าหน้าที่จะแจ้งข้อมูลผ่านทางอีเมลเท่านั้น ขอบคุณครับ";
-        sendAlert($wait, $token, $userId);       
-        break;    
+        sendAlert($wait, $token, $userId);
+        break;
     default:
-        //กรณีที่ไม่มีข้อความตรงกับ Server จะถูกส่งข้อมูล JSON ไปที่ DialogFlow
         callDialogFlow();
     }
-
 } else {
     //กรณีที่ไม่มีข้อความตรงกับ Server จะถูกส่งข้อมูล JSON ไปที่ DialogFlow
     callDialogFlow();
